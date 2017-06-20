8bc0baf0779475e12d2b659a6d485444d89dde1f
LOLITA <LOLITA@lolitadeimac.local>
2017-06-20 14:44:29 +0800

    Initial Commit

---
+++ Drawing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drawing
+//
+//  Created by LOLITA on 17/6/20.
+//  Copyright © 2017年 LOLITA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
